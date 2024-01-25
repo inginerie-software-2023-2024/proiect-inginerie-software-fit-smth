49ae2eff95028e851a2376c849c717163a0ee48b
BiancaGabriela06 <bianca-gabriela.asavoaei@s.unibuc.ro>
2024-01-25 02:13:37 +0200

    Design for landing page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import SidebarMenu from '../components/SidebarMenu'
+import "../css/Home.css"
+import  HomeDesign  from '../components/HomeDesign'
 
 const Home = () => {
     return (
-        <SidebarMenu/>
+        <div>
+             <SidebarMenu/>
+             <HomeDesign/>
+        </div>
+       
+            
     )
 }
 
